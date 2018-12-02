d505e69d22c83c7eff968d0ded3e316384586ab4
unknown <alaguselvan97@gmail.com>
2018-12-03 00:47:21 +0530

    add db seed find by id express nodemon

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const config = require('./config/dev')
-mongoose.connect(config.DB_URI)
+const Rental = require('./models/rental')
+const FakeDb = require('./fake-db')
 
-app.get('/rentals', (req, res)=>{
-    res.json({
-        'success': true
-    })
+const rentalRoutes = require('./routes/rentals')
+
+mongoose.connect(config.DB_URI).then(() => {
+    const fakeDb = new FakeDb()
+    fakeDb.seedDb()
 })
+const app = express()
+
+app.use('/api/v1/rentals',  rentalRoutes)
 
 const chalk = require('chalk')
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 
 app.listen(PORT, () => {
     console.log (chalk.blue(`server running on port ${PORT}`))
